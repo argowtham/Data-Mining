28ef57e5715dd322b960c665439045a0e8a94f11
argowtham <gowtayna@indiana.edu>
2016-08-17 12:10:19 -0400

    Curse of Dimensionality
    Code to understand the curse of dimensionality

---
+++ Curse_of_dimensionality.py
@@ -0,0 +1,33 @@
+import random
+import numpy
+import itertools
+import matplotlib.pyplot as plt
+
+if __name__ == '__main__':
+    print("==================================================")
+    print("Program to demonstrate the curse of dimensionality")
+    print("==================================================\n")
+    # Generating n data points
+    n = int(input("Enter the number of data points to be generated\n"))
+    for l in range(0, 2):
+        data_array = []
+        data_point = []
+        r_array, distance = [], []
+        for k in range(1, 100):
+            for i in range(0, n):
+                for j in range(0, k):
+                    data_point.append(random.random())  # Generating a data point as per the dimensionality required
+                data_array.append(numpy.asarray(data_point))    # Appending all the data points created to a data array
+                data_point = []
+            data_list_array = numpy.asarray(data_array)
+            distance = [numpy.linalg.norm(a-b) for a, b in itertools.combinations(data_list_array, 2)]
+            data_array = []
+            dmax = max(distance)
+            dmin = min(distance)
+            r_array.append(numpy.log10((dmax - dmin)/dmin))
+        plt.plot(r_array)
+        plt.ylabel("r(k) as a function of k")
+        n *= 10
+        print("Increasing n by 10 folds. New n = ", n)
+    plt.show()
+
